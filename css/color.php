c38b564d71d02a566dc06e69d5c7c5d40cbef0fc
T-Ramkumar <Ram90x@gmail.com>
2018-07-03 23:07:07 +0530

    Add files via upload

---
+++ css/color.php
@@ -0,0 +1,133 @@
+
+/*============================================
+
+    'Hex': '#5687bf',  'colorName': 'blue-1'
+
+============================================*/
+.dropdown-menu > .active > a,.dropdown-menu > .active > a:hover{
+  background-color: #0096ff;
+}
+#pricing li:hover .price-inner {
+    border: 1px solid #0096ff;
+}
+#pricing li:hover .btn {
+  background: #0096ff;
+}
+#banner .text-slider h3, .port-over .over-info, .inspectors .teach-over, #testimonial .carousel-indicators li .feeder-name {
+    background: rgba(0, 150, 255,0.8);
+}
+#accordion .panel-default > .panel-heading .panel-title a:before {
+    color: #0096ff;
+}
+#accordion .panel-default > .panel-heading .panel-title a.collapsed:before, .missing-2 .cont .change {
+    color: #0096ff;
+}
+#banner .text-slider .btn {
+    border: 2px solid #fff;
+    background: rgba(0,0,0,0.2);
+}
+#banner .text-slider button.btn {
+    background: #0096ff;
+    border: none;
+}
+#banner .text-slider button.btn:hover {
+    background: rgba(0,0,0,0.5);
+}
+#feature li a:hover {
+    background: rgba(255,255,255,0.2);
+    border-color: rgba(255,255,255,0.2);
+}
+#banner .text-slider .btn:hover {
+    background: #0096ff;
+    border-color: #0096ff;
+}
+.top-bar, header nav li a:hover {
+    border-color: #0096ff;
+}
+.pagination > li > a:hover, .intrested-2 {
+    background: #0096ff;
+}
+#pricing.pricing-2 .price-inner.papu {
+    border-top-color: #0096ff;
+}
+#banner .text-slider p {
+    background: rgba(0,0,0,0.5);
+}
+.tittle h3, #feature li:hover .icon, .port-over .over-info a:hover {
+    color: #0096ff;
+}
+#pricing .price-inner .check-gr i {
+    color: #0096ff !important;
+}
+#pricing .price-inner .btn, .logo a .logo-hex {
+    background: #0096ff;
+}
+#pricing .price-inner .btn:hover {
+    background: #4d4d4d;
+}
+.quote .btn:hover {
+    color: #0096ff;
+}
+header .navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
+    color: #0096ff;
+}
+header .navbar-default .navbar-nav > .active > a, .navbar-default .navbar-nav > .active > a:hover, .navbar-default .navbar-nav > .active > a:focus {
+    border-bottom: 2px solid #0096ff;
+    color: #0096ff;
+}
+.btn, .intrested-2 .intres-lesson .btn {
+    background: #4d4d4d;
+}
+.btn:hover, .intrested-2 .intres-lesson .btn:hover {
+    background: #0096ff;
+}
+.quote, footer button, .intres-lesson .btn, .products .pro-info .btn, .nav-tabs .nav-pills > li.active > a, .nav-pills > li.active > a:hover, .nav-pills > li.active > a:focus {
+    background: #0096ff;
+}
+.products .pro-info .btn-1 {
+    background: #f8f8f8;
+}
+#feature li:hover .inner, .filter li a.active, .filter li a:hover, .products .pro-info .btn-1:hover {
+    background: #0096ff;
+    border-color: #0096ff;
+}
+
+.products .pro-info .btn {
+}
+#pricing.pricing-2 li .price-head {
+    background: none !important;
+}
+
+.products .cate-name .heart li i:hover {
+  color: #0096ff;
+  }
+div#respond p.form-submit input#submit{
+  background: #0096ff;  
+}
+
+.contact-info .social_icons{
+    z-index:1px;
+}
+form#wp-stripe-payment-form .stripe-submit-button {
+    background: #0096ff !important; 
+}
+.where a,.where a i {
+  color: #0096ff;
+}
+
+.top-bar {
+  background: #647382;
+}
+footer .rights {
+  background: #23282d;
+}
+footer{
+  background: #2d3237;
+}
+
+
+body{font-family:''Roboto', sans-serif';font-size:14px !important;color:#647382;}
+
+#header{
+margin: 0 auto;
+}
